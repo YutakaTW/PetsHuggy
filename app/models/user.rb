7c465ea140f64abfa0259d6c3be6c48a543626d2
Yutaka Matsumura <mini.naruchan.ningyou@gmail.com>
2017-07-16 18:57:22 +0800

    add stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :listings
+  has_many :reservations
+
+  has_attached_file :image, :styles => { :medium => "400x400", :thumb => "100x100>" }, :default_url => "avatar-default.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -12,9 +16,14 @@ class User < ActiveRecord::Base
     user.password = Devise.friendly_token[0,20]
     user.name = auth.info.name   # assuming the user model has a name
     user.image = "http://graph.facebook.com/#{auth.uid}/picture?type=large" # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails, 
+    # If you are using confirmable and the provider(s) you use validate emails,
     # uncomment the line below to skip the confirmation emails.
     # user.skip_confirmation!
     end
   end
+
+  def connected?
+    !stripe_user_id.nil?
+  end
+
 end
